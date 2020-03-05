cbbc4cd41736b9c10d76a16ace398c9a1af1947c
SusannaValentina <61779396+SusannaValentina@users.noreply.github.com>
2020-03-05 11:22:55 +0100

    Update main.py

--- Faceplusplus/main.py
+++ Faceplusplus/main.py
@@ -1,4 +1,10 @@
-import Faceplusplus.detect_face as df
+import os
+
+import detect_face as df
 
 # TODO: insert the path_to_my_photo/photo_name.jpg
-df.detect('path_to_my_photo/photo_name.jpg')
+photos = os.listdir(path_to_my_photo)
+
+for photo in photos:
+    df.detect(path_to_my_photo\%s' % photo)
+
